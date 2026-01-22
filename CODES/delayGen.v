0fbd5d6478e0ed6b034c6631822d211eedbefcc0
velmurugan-vlsi <velmuruganr.vlsi@gmail.com>
2026-01-22 14:02:25 +0530

    Update delayGen.v

--- CODES/delayGen.v
+++ CODES/delayGen.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 02/08/2020 08:07:20 PM
-// Design Name: 
-// Module Name: delayGen
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module delayGen(
     input clock,
